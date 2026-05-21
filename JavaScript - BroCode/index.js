19380768c0d656183b790d287edee329a4b5d57a
Sidhesh Tavare <153364937+sidhesh-tavare@users.noreply.github.com>
2026-05-21 15:17:17 +0530

    MOved the repo

---
+++ JavaScript - BroCode/index.js
@@ -0,0 +1,37 @@
+console.log("Hello World!");
+console.log(2+2);
+const a = 90 ;
+console.log("the number is " + a);
+// window.alert("ALERT!");
+// COMMENTS 
+document.getElementById("myH1").textContent="Hello JS typed this " + a;
+document.getElementById("myP").textContent="Hello This is entered using JS ";
+let x;
+x = 100
+console.log(x)
+
+let y = 123;
+console.log(y);
+
+let age = 25;
+let price = 12.99;
+let gpa = 3.4;
+
+console.log(`You are ${age} years old `)
+console.log(`The Price is ${price} `);
+console.log(typeof(age));
+
+let name = "Sidhesh"
+let mname = "Mahesh"
+let lname = "tavare"
+console.log(name + " " + mname.toLocaleUpperCase() + " " + lname.toUpperCase());
+let email = "bro@gmail.com"
+
+let online = false;
+console.log(typeof(online));
+if (online == true){
+    console.log(`Bro is online`);
+}
+else{
+    console.log(`Bro is offline`);
+}
