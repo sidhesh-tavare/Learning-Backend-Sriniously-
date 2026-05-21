77470ea2a55171c708b170141d234a6850fb759a
Sidhesh Tavare <153364937+sidhesh-tavare@users.noreply.github.com>
2026-05-21 16:00:25 +0530

    Learned Basics Calculating circum

--- JavaScript - BroCode/index.js
+++ JavaScript - BroCode/index.js
@@ -1,37 +1,9 @@
-console.log("Hello World!");
-console.log(2+2);
-const a = 90 ;
-console.log("the number is " + a);
-// window.alert("ALERT!");
-// COMMENTS 
-document.getElementById("myH1").textContent="Hello JS typed this " + a;
-document.getElementById("myP").textContent="Hello This is entered using JS ";
-let x;
-x = 100
-console.log(x)
+const PI = 3.14159;
 
-let y = 123;
-console.log(y);
-
-let age = 25;
-let price = 12.99;
-let gpa = 3.4;
-
-console.log(`You are ${age} years old `)
-console.log(`The Price is ${price} `);
-console.log(typeof(age));
-
-let name = "Sidhesh"
-let mname = "Mahesh"
-let lname = "tavare"
-console.log(name + " " + mname.toLocaleUpperCase() + " " + lname.toUpperCase());
-let email = "bro@gmail.com"
-
-let online = false;
-console.log(typeof(online));
-if (online == true){
-    console.log(`Bro is online`);
-}
-else{
-    console.log(`Bro is offline`);
+document.getElementById("submitBTN").onclick = function(){
+    // Create them exactly when and where you need them!
+    const r = document.getElementById("radius").value;
+    const circum = 2 * PI * r;
+    
+    window.alert(`The circumference is ${circum}`);
 }
